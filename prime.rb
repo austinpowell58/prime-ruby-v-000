aeb27192ef0a10c858a3aa9acc4c6fac86184bb6
Austin Powell <austinpowell58@gmail.com>
2018-08-10 16:41:04 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,14 @@ def prime?(number)
   if number <= 1
     return false
   end 
-  x = 2
+  
   small_prime = [2, 3, 5, 7, 11 ]
+  small_prime.each do |primnumber|
+    if primnumber == number
+      return true
+    end 
+  end
+  x = 2
   while x < number/ 2
   if number % x == 0 
     false
