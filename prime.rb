2730af5f13e8f7120f23539dbdfa58f4f499a117
Austin Powell <austinpowell58@gmail.com>
2018-08-10 16:38:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  return false if number <= 1
+  x = 2
+  small_prime = [2, 3, 5, 7, 11 ]
+  while x < number/ 2
+  if number % x == 0 
+    false
+  end 
+  x += 1
+end 
+end
